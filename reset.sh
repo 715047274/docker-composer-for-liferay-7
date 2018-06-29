93ed88e179e8a1fb45a48b8f03a47e4fd8edfa26
Igor Fabiano Nazar <ifnazar@gmail.com>
2018-06-29 14:33:25 -0300

    solr

--- reset.sh
+++ reset.sh
@@ -1,9 +1,12 @@
 docker-compose down --volumes
 
-sudo rm -rf elasticsearch/data
-mkdir elasticsearch/data
-sudo chmod 777 -R elasticsearch
+sudo rm -rf elasticsearch/data/*
+sudo rm -rf elasticsearch/logs/*
+sudo chmod 777 -R elasticsearch/* -R
 
-sudo rm -rf mysql/data
-mkdir mysql/data
-sudo chmod 777 mysql -R
+sudo rm -rf mysql/data/*
+sudo chmod 777 mysql/* -R
+
+sudo rm -rf solr/logs/*
+sudo rm -rf solr/liferay/data/*
+sudo chmod 777 solr/* -R
